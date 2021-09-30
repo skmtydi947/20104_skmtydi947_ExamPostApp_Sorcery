6b3e8a30ca220164ec52284ef34f090261de6adf
坂本雄大 <skmtydi947@sakamotoyuudainoMacBook-Pro.local>
2021-10-01 03:06:03 +0900

    Add: フラッシュメッセージの設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
 
-private
+  private
+  
   def not_authenticated
     redirect_to login_path, warning: 'ログインしてください'
   end
