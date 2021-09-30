dbf9bfcd5f4715e1649cc632e1c3dc6a608cf89f
坂本雄大 <skmtydi947@sakamotoyuudainoMacBook-Pro.local>
2021-10-01 04:11:40 +0900

    構文修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
-private
+  private
+
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
